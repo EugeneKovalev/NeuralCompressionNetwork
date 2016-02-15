3a0b6568bbb07c6a0d63c05e7dbad956d03e90ea
Anders Breivik <cyberthrone@tut.by>
2016-02-15 15:34:09 +0300

    BS

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PIL import Image
-from methods import divide_to_particles, get_rgb_values, get_rgb_matrixes, get_zero_layer_weights_matrix, \
-    get_alpha, get_green_output_values, get_blue_output_values
+from methods import divide_to_particles, get_rgb_values, get_rgb_matrixes, get_color_output_values, \
+    process_teaching_for_color
 
 image = Image.open("Test_Image.png").convert("RGB")
 image_size = image.size
@@ -18,67 +18,59 @@ rgb_matrixes = get_rgb_matrixes(rgb_values)
 first_layer_neurons_number = input('Enter the number of neurons in the first layer: ')
 MAX_ERROR = input('Enter the MAX ERROR value: ')
 
-zero_layer_neurons_number = rgb_matrixes[0]['red'].size
+RED_zero_layer_weights, RED_first_layer_weights = process_teaching_for_color(MAX_ERROR, first_layer_neurons_number, rgb_matrixes, 'red')
 
-list_of_corrected_weights = []
+GREEN_zero_layer_weights, GREEN_first_layer_weights = process_teaching_for_color(MAX_ERROR, first_layer_neurons_number, rgb_matrixes, 'green')
 
-# the next string initializes a do-while-like loop
-mean_square_error = MAX_ERROR + 1
-error_iteration = 0
-while mean_square_error > MAX_ERROR:
-    matrix_iteration = 0
-    list_of_delta_matrixes = []
-    for rgb_matrix in rgb_matrixes:
-        if error_iteration == 0:
-            zero_layer_weights_matrix = get_zero_layer_weights_matrix(zero_layer_neurons_number, first_layer_neurons_number)
-            first_layer_weights_matrix = zero_layer_weights_matrix.H
-        else:
-            zero_layer_weights_matrix = list_of_corrected_weights['zero_layer']
-            first_layer_weights_matrix = list_of_corrected_weights['first_layer']
-        ALPHA = get_alpha(rgb_matrix['red'])
-        first_layer_output_values = rgb_matrix['red'] * zero_layer_weights_matrix
-        second_layer_output_values = first_layer_output_values * first_layer_weights_matrix
-        ALPHA_QUOTE = get_alpha(first_layer_output_values)
-        delta_values_matrix = second_layer_output_values - rgb_matrix['red']
-        corrected_first_layer_weights_matrix = first_layer_weights_matrix - \
-                                               ALPHA_QUOTE * first_layer_output_values.H * delta_values_matrix
-        corrected_zero_layer_weights_matrix = zero_layer_weights_matrix - \
-                                              ALPHA * rgb_matrix['red'].H * \
-                                              delta_values_matrix * first_layer_weights_matrix.H
-        list_of_delta_matrixes.append(delta_values_matrix)
-        list_of_corrected_weights = {'zero_layer': corrected_zero_layer_weights_matrix,
-                                     'first_layer': corrected_first_layer_weights_matrix}
-        matrix_iteration += 1
-
-    mean_square_error = 0
-    for delta_matrix in list_of_delta_matrixes:
-        mean_square_error += delta_matrix * delta_matrix.H
-
-    print('MSE ', mean_square_error)
-    print(error_iteration)
-    error_iteration += 1
+BLUE_zero_layer_weights, BLUE_first_layer_weights = process_teaching_for_color(MAX_ERROR, first_layer_neurons_number, rgb_matrixes, 'blue')
 
 print('JESUS CHRIST')
 
+new_rgb_values = {'red': None, 'green': None, 'blue': None}
 
-print('Holly Molly')
-print('MAX ERROR', mean_square_error, ' < ', MAX_ERROR)
-print(matrix_iteration, "====================================")
+for rgb_matrix in rgb_matrixes:
+    RED_latent_layer = rgb_matrix['red'] * RED_zero_layer_weights
+    RED_output_layer = RED_latent_layer * RED_first_layer_weights
+    GREEN_latent_layer = rgb_matrix['green'] * GREEN_zero_layer_weights
+    GREEN_output_layer = GREEN_latent_layer * GREEN_first_layer_weights
+    BLUE_latent_layer = rgb_matrix['blue'] * BLUE_zero_layer_weights
+    BLUE_output_layer = BLUE_latent_layer * BLUE_first_layer_weights
+    for r in list(RED_output_layer.flat):
+        print(int((r + 1) * 255 / 2))
+    for g in list(GREEN_output_layer.flat):
+        print(int((g + 1) * 255 / 2))
+    for b in list(BLUE_output_layer.flat):
+        print(int((b + 1) * 255 / 2))
 
-RED_second_layer_output_values = second_layer_output_values
-GREEN_second_layer_output_values = get_green_output_values(rgb_matrix['green'], zero_layer_weights_matrix)
-BLUE_second_layer_output_values = get_blue_output_values(rgb_matrix['blue'], zero_layer_weights_matrix)
-new_data_iteration = 0
-for r, g, b in newData[matrix_iteration * 2:(matrix_iteration + 1) * 2]:  # FIVE
-    RED_changed_color_value = RED_second_layer_output_values.flat[new_data_iteration]
-    RED_changed_color_value = int((RED_changed_color_value + 1) * 255 / 2)
-    GREEN_changed_color_value = GREEN_second_layer_output_values.flat[new_data_iteration]
-    GREEN_changed_color_value = int((GREEN_changed_color_value + 1) * 255 / 2)
-    BLUE_changed_color_value = BLUE_second_layer_output_values.flat[new_data_iteration]
-    BLUE_changed_color_value = int((BLUE_changed_color_value + 1) * 255 / 2)
-    newData[matrix_iteration * 2 + new_data_iteration] = RED_changed_color_value, GREEN_changed_color_value, BLUE_changed_color_value  # FIVE
-    new_data_iteration += 1
-matrix_iteration += 1
+
+def divide_to_particles(image_size, pixels):
+    particles = []
+    particle = []
+    k, v = image_size
+    for j in range(0, v, 2):
+        for i in range(k*j, k*(j+1)):
+            particle.append(pixels[i])
+            if (i+1) % 2 == 0:
+                particle.append(pixels[k+i-1])
+                particle.append(pixels[k+i])
+                particles.append(particle)
+                particle = []
+    return particles
+
+#RSLA = get_color_output_values(rgb_matrix['red'], zero_layer_weights_matrix)
+#print(RSLA)
+#GREEN_second_layer_output_values = get_color_output_values(rgb_matrix['green'], zero_layer_weights_matrix)
+#BLUE_second_layer_output_values = get_blue_output_values(rgb_matrix['blue'], zero_layer_weights_matrix)
+#new_data_iteration = 0
+#for r, g, b in newData:  # FIVE
+    #RED_changed_color_value = RED_second_layer_output_values.flat[new_data_iteration]
+    #RED_changed_color_value = int((RED_changed_color_value + 1) * 255 / 2)
+    #GREEN_changed_color_value = GREEN_second_layer_output_values.flat[new_data_iteration]
+    #GREEN_changed_color_value = int((GREEN_changed_color_value + 1) * 255 / 2)
+    #BLUE_changed_color_value = BLUE_second_layer_output_values.flat[new_data_iteration]
+    #BLUE_changed_color_value = int((BLUE_changed_color_value + 1) * 255 / 2)
+    #r,g,b = RED_changed_color_value, 0, 0
+    #new_data_iteration += 1
 
 image.putdata(newData)
 image.save('out.jpg')
--- methods.py
+++ methods.py
@@ -56,12 +56,77 @@ def get_alpha(matrix):
         values_to_be_squared += i*i
     return 1/values_to_be_squared
 
-def get_green_output_values(green_values, zero_layer_weights):
-    first_layer_output_values = green_values*zero_layer_weights
+
+def get_color_output_values(color_values, zero_layer_weights):
+    first_layer_output_values = color_values*zero_layer_weights
     first_layer_weights_matrix = zero_layer_weights.H
     return first_layer_output_values*first_layer_weights_matrix
 
-def get_blue_output_values(blue_values, zero_layer_weights):
-    first_layer_output_values = blue_values*zero_layer_weights
-    first_layer_weights_matrix = zero_layer_weights.H
-    return first_layer_output_values*first_layer_weights_matrix
+
+def process_teaching_for_color(MAX_ERROR, first_layer_neurons_number, rgb_matrixes, color):
+    zero_layer_neurons_number = rgb_matrixes[0]['red'].size
+
+    corrected_weights = []
+
+    # the next string initializes a do-while-like loop
+    mean_square_error = MAX_ERROR + 1
+    error_iteration = 0
+    while mean_square_error > MAX_ERROR:
+        matrix_iteration = 0
+        list_of_delta_matrixes = []
+        for rgb_matrix in rgb_matrixes:
+
+            if error_iteration == 0:
+                zero_layer_weights_matrix = get_zero_layer_weights_matrix(zero_layer_neurons_number, first_layer_neurons_number)
+                first_layer_weights_matrix = zero_layer_weights_matrix.H
+            else:
+                zero_layer_weights_matrix = corrected_weights['zero_layer']
+                first_layer_weights_matrix = corrected_weights['first_layer']
+
+
+            """
+            if error_iteration == 0:
+                zero_layer_weights_matrix = get_zero_layer_weights_matrix(zero_layer_neurons_number, first_layer_neurons_number)
+                first_layer_weights_matrix = zero_layer_weights_matrix.H
+            else:
+                zero_layer_weights_matrix = corrected_weights[matrix_iteration]['zero_layer']
+                first_layer_weights_matrix = corrected_weights[matrix_iteration]['first_layer']
+            """
+
+            ALPHA = get_alpha(rgb_matrix[color])
+            first_layer_output_values = rgb_matrix[color] * zero_layer_weights_matrix
+            second_layer_output_values = first_layer_output_values * first_layer_weights_matrix
+            ALPHA_QUOTE = get_alpha(first_layer_output_values)
+            delta_values_matrix = second_layer_output_values - rgb_matrix[color]
+            corrected_first_layer_weights_matrix = first_layer_weights_matrix - \
+                                                   ALPHA_QUOTE * first_layer_output_values.H * delta_values_matrix
+            corrected_zero_layer_weights_matrix = zero_layer_weights_matrix - \
+                                                  ALPHA * rgb_matrix[color].H * \
+                                                  delta_values_matrix * first_layer_weights_matrix.H
+            list_of_delta_matrixes.append(delta_values_matrix)
+
+            corrected_weights = {'zero_layer': corrected_zero_layer_weights_matrix,
+                                         'first_layer': corrected_first_layer_weights_matrix}
+
+
+            """
+            if error_iteration == 0:
+                corrected_weights.append({'zero_layer': corrected_zero_layer_weights_matrix,
+                                                    'first_layer': corrected_first_layer_weights_matrix})
+            else:
+                corrected_weights.pop(matrix_iteration)
+                corrected_weights.insert(matrix_iteration, {'zero_layer': corrected_zero_layer_weights_matrix,
+                                                                    'first_layer': corrected_first_layer_weights_matrix})
+            """
+
+            matrix_iteration += 1
+
+        mean_square_error = 0
+        for delta_matrix in list_of_delta_matrixes:
+            mean_square_error += delta_matrix * delta_matrix.H
+
+        print('MSE ', mean_square_error)
+        print(error_iteration)
+        error_iteration += 1
+    print(matrix_iteration, "====================================")
+    return corrected_zero_layer_weights_matrix, corrected_first_layer_weights_matrix
